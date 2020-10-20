badd42480937abfba5e9a550e8202e8018270b77
CarlosJair117 <carlos.fdez.guar19@gmail.com>
2020-10-20 00:58:14 -0500

    tercer commit

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Movie } from 'src/app/interfaces/catelera-response';
+import { PeliculasService } from 'src/app/services/peliculas.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  
+  public movies: Movie [] = [];
 
-  constructor() { }
+  constructor( private peliculasService: PeliculasService) { }
 
   ngOnInit(): void {
+
+    this.peliculasService.getCartelera()
+    .subscribe( resp => {
+      //console.log(resp);
+      this.movies = resp.results;
+    });
   }
 
 }
